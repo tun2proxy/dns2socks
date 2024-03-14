f54f41930fcbdea9fdc74b310d9aa2c4db6e36a8
ssrlive <30760636+ssrlive@users.noreply.github.com>
2024-03-14 18:47:40 +0800

    Bump version 0.1.2

---
+++ build-apple.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+
+echo "Setting up the rust environment..."
+rustup target add aarch64-apple-ios aarch64-apple-ios-sim x86_64-apple-ios x86_64-apple-darwin aarch64-apple-darwin
+cargo install cbindgen
+
+echo "Building..."
+cargo build --release --target x86_64-apple-darwin
+cargo build --release --target aarch64-apple-darwin
+cargo build --release --target aarch64-apple-ios
+cargo build --release --target x86_64-apple-ios
+cargo build --release --target aarch64-apple-ios-sim
+
+echo "Generating includes..."
+mkdir -p target/include/
+cbindgen --config cbindgen.toml -l C -o target/include/dns2socks.h
+cat > target/include/module.modulemap <<EOF
+framework module dns2socks {
+    umbrella header "dns2socks.h"
+
+    export *
+    module * { export * }
+}
+EOF
+
+echo "lipo..."
+echo "Simulator"
+lipo -create \
+    target/aarch64-apple-ios-sim/release/libdns2socks.a \
+    target/x86_64-apple-ios/release/libdns2socks.a \
+    -output ./target/libdns2socks-ios-sim.a
+
+echo "MacOS"
+lipo -create \
+    target/aarch64-apple-darwin/release/libdns2socks.a \
+    target/x86_64-apple-darwin/release/libdns2socks.a \
+    -output ./target/libdns2socks-macos.a
+
+echo "Creating XCFramework"
+rm -rf ./dns2socks.xcframework
+xcodebuild -create-xcframework \
+    -library ./target/aarch64-apple-ios/release/libdns2socks.a -headers ./target/include/ \
+    -library ./target/libdns2socks-ios-sim.a -headers ./target/include/ \
+    -library ./target/libdns2socks-macos.a -headers ./target/include/ \
+    -output ./dns2socks.xcframework
